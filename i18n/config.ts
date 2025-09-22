8454b419fac633c90224a7edd4dc75ddfe5008f1
Yassine Bouhlal <106955041+bouhlalyassine@users.noreply.github.com>
2025-09-22 10:35:57 +0100

    Delete i18n directory

--- i18n/config.ts
+++
@@ -1,8 +0,0 @@
-export const defaultLocale = 'fr';
-export const locales = ['fr', 'en'] as const;
-export type AppLocale = (typeof locales)[number];
-
-export const localeNames: Record<AppLocale, string> = {
-  fr: 'Français',
-  en: 'English'
-};
